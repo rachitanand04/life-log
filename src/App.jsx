4b14f6c973e82ff03d1611c709cf3d6d7924958a
rachitanand04 <rachit.anand04111@outlook.com>
2026-03-20 16:55:18 +0530

    Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./Pages/Home";
 
-function App(){
-    return(
-        <Routes>
-            <Route path="/" element={<Home/>} />
-        </Routes>
-    )
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
+
+
+//Color Pallete: https://colorhunt.co/palette/fff8f0c085528c5a3c4b2e2b
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,15 @@
-function Home(){
-    return(
-        <h1> Home Page </h1>
-    )
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Life Logger</h1>
+      <div className="buttons">
+        <button id="login">Login</button>
+        <button id="sign-up">Sign Up</button>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
