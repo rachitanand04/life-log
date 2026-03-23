b135b460d74e1e74a8d96bcf0c6ae295ab92627a
rachitanand04 <rachit.anand04111@outlook.com>
2026-03-23 15:44:20 +0530

    Log structure changes

--- client/src/Components/Dashboard/Log.jsx
+++ client/src/Components/Dashboard/Log.jsx
@@ -3,9 +3,17 @@ import LogEntry from "./LogEntry";
 function Log(props) {
   return (
     <div className="log">
-      {props.entries.map(function (entry) {
-        return <LogEntry key={entry.id} type={entry.type} content={entry.content} />
-      })}
+      <div className="log-list">
+        {props.entries.map(function (entry) {
+          return (
+            <LogEntry
+              key={entry.id}
+              type={entry.type}
+              content={entry.content}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
