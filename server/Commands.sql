ad76dfbb93c565c9761005eb78eabfbe65397bbc
rachitanand04 <rachit.anand04111@outlook.com>
2026-04-13 14:22:31 +0530

    SQL command update

--- server/Commands.sql
+++ server/Commands.sql
@@ -5,13 +5,13 @@ email VARCHAR(40) NOT NULL,
 password VARCHAR(80) NOT NULL
 );
 
+-- To create the logs tabel
 CREATE TABLE logs( 
     id SERIAL PRIMARY KEY, 
     email VARCHAR(40) NOT NULL, 
     type VARCHAR(15) NOT NULL, 
     content TEXT, 
-    ime_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+    time_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
     status TEXT, 
-    scheduled_time TIMESTAMP, 
     due_date TIMESTAMP 
 );
